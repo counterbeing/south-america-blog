dd26960b2b964d08cc326504ae9bb6f10246a2aa
Cory Logan <he@corylogan.com>
2017-07-22 15:57:08 +0200

    adjust pointers

--- app/models/destination.js
+++ app/models/destination.js
@@ -48,8 +48,8 @@ export default Model.extend({
       url: `/${this.get('color')}-marker.png`,
       size: new window.google.maps.Size(62, 62),
       origin: new window.google.maps.Point(0, 0),
-      anchor: new window.google.maps.Point(15, 30),
-      scaledSize: new window.google.maps.Size(30, 30),
+      anchor: new window.google.maps.Point(8, 30),
+      scaledSize: new window.google.maps.Size(18, 30),
     }
   }),
 
